c6795b695afb1b9173bfa923087529ec52f9b954
Lollipop <291950304@qq.com>
2021-07-18 22:21:10 +0800

    退出登录时重置路由

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,12 +2,13 @@
  * @Author: fan
  * @Date: 2021-06-28 19:34:01
  * @LastEditors: fan
- * @LastEditTime: 2021-06-29 18:19:37
+ * @LastEditTime: 2021-07-18 22:19:43
  * @Description: 用户的状态
  */
 
 import { login, getUserInfo, getUserDetailById } from '@/api/user'
 import { getToken, setToken, removeToken, setTimeStamp } from '@/utils/auth'
+import { resetRouter } from '@/router'
 // 状态
 const state = {
   token: getToken(),
@@ -75,6 +76,14 @@ const actions = {
     context.commit('removeToken') // 不仅仅删除了vuex中的 还删除了缓存中的
     // 删除用户信息
     context.commit('removeUserInfo')
+    // 重置路由
+    resetRouter()
+    // 还有一步  vuex中的数据是不是还在
+    // 要清空permission模块下的state数据
+    // vuex中 user子模块  permission子模块
+    // 子模块调用子模块的action  默认情况下 子模块的context是子模块的
+    // 父模块 调用 子模块的action
+    context.commit('permission/setRoutes', [], { root: true })
   }
 }
 
